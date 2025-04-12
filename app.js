9715cd76174af26cf1f0f917ea0e5a201ce3b5dd
Akash <akash.v.harale@gmail.com>
2025-04-12 21:14:38 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+cosnt;
+app.get("/", (req, res) => {
+  res.status(200).send("Home Page");
+});
+const server = app.listen(PORT);
+
+server.on("error", (err) => {
+  console.log(`failed to start the server ${err.message}`);
+});
