f09a784fbc6e7374f0f1f1cd81cf09946890b283
Akash <akash.v.harale@gmail.com>
2025-04-16 17:27:44 +0530

    feat : added delete functionlity

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -16,8 +16,24 @@ todoRouter.post("/add", async (req, res) => {
   try {
     const todo = new TodoModel(req.body);
     await todo.save();
-    res.send({ message: "A new Todo has been added" });
+    res.send(todo);
   } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
+
+// Add DELETE endpoint to remove a todo by ID
+todoRouter.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(`Attempting to delete todo with id: ${id}`); // Debug log
+    const deletedTodo = await TodoModel.findByIdAndDelete(id);
+    if (!deletedTodo) {
+      return res.status(404).send({ message: "Todo not found" });
+    }
+    res.send({ message: "Todo deleted successfully" });
+  } catch (error) {
+    console.log(`Error deleting todo: ${error.message}`); // Debug log
     res.status(400).send({ message: error.message });
   }
 });
